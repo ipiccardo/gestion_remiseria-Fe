326445133ed0a8a72fc444fc04b547ef98d0bdc1
ipiccard <ivan.piccardo@tsoftlatam.com>
2024-05-06 09:48:17 -0300

    Se modifica el bug del home en responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <div className="font-sans">
 
-      <div className="bg-[url('/images/portada.jpg')] max-h-screen absolute top-0 left-0 right-0 bottom-0 bg-no-repeat bg-cover">
+      <div className="bg-[url('/images/portada.jpg')] h-screen w-screen absolute top-0 left-0 right-0 bottom-0 bg-no-repeat bg-cover">
       </div>
 
-      <div className="absolute inset-0 flex flex-col justify-center max-h-screen items-center bg-black bg-opacity-50 text-white p-8 z-40">
+      <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-50 text-white p-8 z-40">
         <h1 className="text-4xl md:text-6xl font-bold text-center mb-8">Welcome to our car service</h1>
         <h2 className="text-4xl md:text-6xl font-bold text-center mb-8">Journey with confidence, ride with ease</h2>
         <Link href="/dashboard/drivers"
