4b47414e7b1d3363467b8b963565602d28e68d04
ipiccard <ivan.piccardo@tsoftlatam.com>
2024-05-06 20:46:02 -0300

    Se modifica el padding de layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <h1 className="text-4xl md:text-6xl font-bold text-center mb-8">Welcome to our car service</h1>
         <h2 className="text-4xl md:text-6xl font-bold text-center mb-8">Journey with confidence, ride with ease</h2>
         <Link href="/dashboard/drivers"
-          className="bg-blue-700 text-white py-4 px-4 rounded-lg hover:bg-blue-800 transition duration-300 flex justify-center mt-3.5 text-5xl items-center">Enter
+          className="bg-blue-700 text-white py-4 px-8 rounded-lg hover:bg-blue-800 transition duration-300 flex justify-center mt-3.5 text-3xl md:text-5xl items-center ">Enter
         </Link>
       </div>
     </div>
